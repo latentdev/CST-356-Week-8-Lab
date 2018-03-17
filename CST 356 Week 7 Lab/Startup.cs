f38d4afa41127817b60ba82e1d0944cd3a1104cb
Nicholas Olson <latentdev@gmail.com>
2018-03-16 20:39:13 -0700

    Add project files.

---
+++ CST 356 Week 7 Lab/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(CST_356_Week_7_Lab.Startup))]
+namespace CST_356_Week_7_Lab
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
